ad16f550a14f1b74a0fb9775b44fd94019a77b79
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-15 17:43:00 -0400

    Edited possible report channels

--- commands/report.js
+++ commands/report.js
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Time", message.createdAt)
     .addField("Reason", rreason);
 
-    let reportschannel = message.guild.channels.find(`name`, "reports");
+    let reportschannel = message.guild.channels.find(`name`, "reports", "alerts", "report");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
     reportschannel.send(reportEmbed);
 
