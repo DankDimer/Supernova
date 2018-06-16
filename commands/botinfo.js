5a3086ce147f1fb0a023eb0e92de1fc1cc132da5
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-16 19:24:12 -0400

    test

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,12 +3,13 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
+    let creator = "DankDimer#2285"
     .setDescription("Bot Information")
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Created On", bot.user.createdAt)
-    .addField("Created By:", "DankDimer#2285");
+    .addField("Created By:", creator);
 
     message.channel.send(botembed);
 }
