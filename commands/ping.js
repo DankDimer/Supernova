7926d3b3ddc526aba8bca04a5e11c1a2e4611a3f
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-16 10:46:45 -0400

    got rid of ping command bc it was broken

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js")
-
-if(command === "ping") {
-
-
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-      }
