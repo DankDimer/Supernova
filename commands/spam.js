6e19c09b8e9d51ecf3055862b22ff35392c5f5f8
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-23 20:14:47 -0400

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -1,1 +1,28 @@
 
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+
+if(command === "spam") {
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone")
+  message.reply("@everyone");
+}
+
+module.exports.help = {
+  name: "spam"
+}
