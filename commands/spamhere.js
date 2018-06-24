bfd5f97fc79f4bf36923419e5d3f01e0361d0c58
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-24 09:35:24 -0400

    Create spamhere.js

---
+++ commands/spamhere.js
@@ -0,0 +1,27 @@
+const Discord = require ("discord.js")
+module.exports.run = async (bot, message, args) => {
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+message.reply("@here")
+}
+
+module.exports.help = {
+  name:"botinfo"
+}
