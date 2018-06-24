f49cbfe02377bfe9e05f08e3dcfda05db5034d84
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-24 09:31:14 -0400

    Create spam.js

---
+++ commands/spam.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+message.reply("@everyone")
+}
+
+module.exports.help = {
+  name:"spam"
+}
