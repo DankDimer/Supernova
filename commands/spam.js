aade5045e38ac5ea447231a41305daedb45d13e6
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-06-24 09:53:05 -0400

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+      if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
 message.reply("@everyone")
 message.reply("@everyone")
 message.reply("@everyone")
