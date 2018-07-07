34602923fca667ada47144e8e36b5f3a4cd5158c
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-07-07 13:32:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -150,6 +150,8 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
   setTimeout(() => {
     cooldown.delete(message.author.id)
