f0bd5d26b9c2924dae1ac639ae744e8bdb34e550
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-07-07 13:48:47 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,7 @@
   
   const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
 
   module.exports.run = async (bot, message, args) => {
   
