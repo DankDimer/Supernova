5b27702a097f424019bd775f9fec9fd7ba02162b
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-07-07 13:56:13 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -6,13 +6,13 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
   if(!args[0] || args[0 == "help"]) return message.reply("Usage: !prefix <desired prefix here>");
 
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  let prefixes = JSON.parse(fs.readFileSync("/../prefixes.json", "utf8"));
 
   prefixes[message.guild.id] = {
     prefixes: args[0]
   };
 
-  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+  fs.writeFile("/../prefixes.json", JSON.stringify(prefixes), (err) => {
     if (err) console.log(err)
   });
 
