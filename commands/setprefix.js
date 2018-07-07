2d8689c2b3864c4aef5a619af43651da944b0d6f
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-07-07 13:52:36 -0400

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,4 +1,7 @@
 const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
+const fs = requre("fs")
 
 module.exports.run = async (bot, message, args) => {
     //!setprefix <symbol>
@@ -6,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("🚫***You can not do that.***");
     if(!args[0] || args[0 == "helpme"]) return message.reply("Usage: !prefix <prefered prefix> *Only one is reccomended*");
 
-    let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
+    let prefixes = JSON.parse(fs.readFileSync("/../prefix.json", "utf8"));
 
     prefixes[message.guild.id] = {
         prefixes: args[0]
